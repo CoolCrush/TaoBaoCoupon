cda49f11d607cc1638828b9ceacfbed962c11213
coolcr <876743742@qq.com>
2021-05-24 15:31:22 +0800

    完成精选页面左侧分类的列表加载

---
+++ app/src/main/java/com/coolcr/taobaocoupon/ui/adapter/SelectedPageLeftAdapter.java
@@ -0,0 +1,61 @@
+package com.coolcr.taobaocoupon.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.coolcr.taobaocoupon.R;
+import com.coolcr.taobaocoupon.model.domain.SelectedPageCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectedPageLeftAdapter extends RecyclerView.Adapter<SelectedPageLeftAdapter.InnerHolder> {
+
+    private List<SelectedPageCategory.DataBean> mData = new ArrayList<>();
+
+    //当前选中那个分类
+    private int mCurrentSelectedPosition = 0;
+
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_selected_page_left, parent, false);
+        return new InnerHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
+        TextView categoryTv = holder.itemView.findViewById(R.id.left_category_tv);
+        if (mCurrentSelectedPosition == position) {
+            categoryTv.setBackgroundColor(categoryTv.getResources().getColor(R.color.colorTvSelected));
+        } else {
+            categoryTv.setBackgroundColor(categoryTv.getResources().getColor(R.color.white));
+        }
+        SelectedPageCategory.DataBean dataBean = mData.get(position);
+        categoryTv.setText(dataBean.getFavorites_title());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void setData(SelectedPageCategory categories) {
+        mData.clear();
+        if (mData != null) {
+            mData.addAll(categories.getData());
+            notifyDataSetChanged();
+        }
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/coolcr/taobaocoupon/ui/fragment/SelectedFragment.java
+++ app/src/main/java/com/coolcr/taobaocoupon/ui/fragment/SelectedFragment.java
@@ -3,24 +3,36 @@ package com.coolcr.taobaocoupon.ui.fragment;
 import android.view.View;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.coolcr.taobaocoupon.R;
 import com.coolcr.taobaocoupon.base.BaseFragment;
 import com.coolcr.taobaocoupon.model.domain.SelectedContent;
 import com.coolcr.taobaocoupon.model.domain.SelectedPageCategory;
 import com.coolcr.taobaocoupon.presenter.impl.SelectedPagePresenterImpl;
+import com.coolcr.taobaocoupon.ui.adapter.SelectedPageLeftAdapter;
 import com.coolcr.taobaocoupon.utils.LogUtils;
 import com.coolcr.taobaocoupon.utils.PresenterManger;
 import com.coolcr.taobaocoupon.view.ISelectedCallback;
 
 import java.util.List;
 
+import butterknife.BindView;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SelectedFragment extends BaseFragment implements ISelectedCallback {
 
+    @BindView(R.id.left_category_list)
+    RecyclerView leftCategoryList;
+
+    @BindView(R.id.content_list)
+    RecyclerView contentList;
+
     private SelectedPagePresenterImpl mSelectedPagePresenter;
+    private SelectedPageLeftAdapter mLeftAdapter;
 
     @Override
     protected void initPresenter() {
@@ -46,13 +58,18 @@ public class SelectedFragment extends BaseFragment implements ISelectedCallback 
     @Override
     protected void initView(View rootView) {
         setUpState(State.SUCCESS);
+        leftCategoryList.setLayoutManager(new LinearLayoutManager(getContext()));
+        mLeftAdapter = new SelectedPageLeftAdapter();
+        leftCategoryList.setAdapter(mLeftAdapter);
     }
 
     @Override
     public void onCategoriesLoad(SelectedPageCategory categories) {
         //分类内容返回
         LogUtils.d(this, "categories -- > " + categories.toString());
+        mLeftAdapter.setData(categories);
         List<SelectedPageCategory.DataBean> data = categories.getData();
+
         mSelectedPagePresenter.getContentByCategory(data.get(0));
     }
 
