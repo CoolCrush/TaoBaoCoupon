e407219dca2b2dc864239e958f5a3032d64078bc
coolcr <876743742@qq.com>
2021-05-26 10:28:15 +0800

    完成搜索页面的保存历史记录的逻辑编写

--- app/src/main/java/com/coolcr/taobaocoupon/presenter/impl/SearchPresenterImpl.java
+++ app/src/main/java/com/coolcr/taobaocoupon/presenter/impl/SearchPresenterImpl.java
@@ -1,14 +1,18 @@
 package com.coolcr.taobaocoupon.presenter.impl;
 
 import com.coolcr.taobaocoupon.model.Api;
+import com.coolcr.taobaocoupon.model.domain.Histories;
 import com.coolcr.taobaocoupon.model.domain.HotWordsContent;
 import com.coolcr.taobaocoupon.model.domain.SearchResult;
 import com.coolcr.taobaocoupon.presenter.ISearchPresenter;
+import com.coolcr.taobaocoupon.utils.JsonCacheUtil;
 import com.coolcr.taobaocoupon.utils.LogUtils;
 import com.coolcr.taobaocoupon.utils.RetrofitManager;
 import com.coolcr.taobaocoupon.view.ISearchCallback;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,25 +26,72 @@ public class SearchPresenterImpl implements ISearchPresenter {
 
     private int mCurrentPage = 1;
     private String mCurrentKeyword = null;
+    private final JsonCacheUtil mJsonCacheUtil;
 
     public SearchPresenterImpl() {
         Retrofit retrofit = RetrofitManager.getInstance().getRetrofit();
         mApi = retrofit.create(Api.class);
+        mJsonCacheUtil = JsonCacheUtil.getInstance();
     }
 
     @Override
     public void getHistories() {
-
+        Histories histories = mJsonCacheUtil.getValue(KEY_HISTORIES, Histories.class);
+        if (mViewCallback != null &&
+                histories != null &&
+                histories.getHistories() != null &&
+                histories.getHistories().size() != 0) {
+            mViewCallback.onHistoriesLoaded(histories.getHistories());
+        }
     }
 
     @Override
     public void delHistories() {
+        mJsonCacheUtil.delCache(KEY_HISTORIES);
+    }
 
+    public static final String KEY_HISTORIES = "key_histories";
+
+    private int historiesMaxSize = 10;
+
+    /**
+     * 添加历史记录
+     *
+     * @param history
+     */
+    private void saveHistory(String history) {
+        //如果说已经存在了，就删掉再添加
+        Histories histories = mJsonCacheUtil.getValue(KEY_HISTORIES, Histories.class);
+        List<String> historiesList = null;
+        //去重
+        if (histories != null && histories.getHistories() != null) {
+            historiesList = histories.getHistories();
+            if (historiesList.contains(history)) {
+                historiesList.remove(history);
+            }
+        }
+        //处理没有数据的情况
+        if (histories == null) {
+            histories = new Histories();
+        }
+        if (historiesList == null) {
+            historiesList = new ArrayList<>();
+        }
+        histories.setHistories(historiesList);
+        if (historiesList.size() > historiesMaxSize) {
+            historiesList.subList(0, historiesMaxSize);
+        }
+        //添加记录
+        historiesList.add(history);
+
+        mJsonCacheUtil.saveCache(KEY_HISTORIES, historiesList);
     }
 
     @Override
     public void doSearch(String keyword) {
         this.mCurrentKeyword = keyword;
+        this.saveHistory(keyword);
+        
         if (mViewCallback != null) {
             mViewCallback.onLoading();
         }
